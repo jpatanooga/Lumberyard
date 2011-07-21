f85354b6515129a9c51767f3432d7cbdd5a49f76
Josh Patterson <jpatterson@floe.tv>
2011-07-20 22:40:24 -0500

    added license to all files, updated hbase to cd3u0

--- src/tv/floe/lumberyard/hbase/isax/index/HBaseUtils.java
+++ src/tv/floe/lumberyard/hbase/isax/index/HBaseUtils.java
@@ -1,3 +1,20 @@
+/**
+   Copyright [2011] [Josh Patterson]
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+ */
+
 package tv.floe.lumberyard.hbase.isax.index;
 
 import java.io.IOException;
@@ -8,6 +25,7 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.MasterNotRunningException;
+import org.apache.hadoop.hbase.ZooKeeperConnectionException;
 import org.apache.hadoop.hbase.client.Delete;
 import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
@@ -162,6 +180,9 @@ public class HBaseUtils {
 		} catch (MasterNotRunningException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
+		} catch (ZooKeeperConnectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 		if ( null == admin) {
